b4036fc19eda9fa4c67208d8a9aadc7d31864602
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-01-30 17:25:23 +0530

    counter interview question

--- Project1/Firstproject/src/App.jsx
+++ Project1/Firstproject/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     const addvalue=()=>{
       // counter=counter+1
       setCounter(counter+1)
+      setCounter(prevCounter=> prevCounter+1)
+      setCounter(counter=>counter+1)
     }
     const removeValue=()=>{
       // counter= counter-1
