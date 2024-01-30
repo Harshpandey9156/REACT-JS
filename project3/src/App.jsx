315062353dfde9628e6193359eddebaea2fa81cb
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-01-30 16:15:07 +0530

    tailwind image processor

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -8,7 +8,28 @@ function App() {
 
   return (
     <>
-       <h1 className='bg-color400'>Tailwind test</h1>
+       <h1 className='bg-green-400 text-black  p-4 rounded-xl'>Tailwind test</h1>
+
+       <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
 
     </>
   )
