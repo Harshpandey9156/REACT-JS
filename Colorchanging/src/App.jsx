eb6989eab0d77ee2ad39b70485ba4203a83ee8a6
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-01-30 17:47:22 +0530

    color changing

---
+++ Colorchanging/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [Color,setColor]=useState ("white")
+
+  return (
+    <>
+       <div className='W-full'></div>
+    </>
+  )
+}
+
+export default App
