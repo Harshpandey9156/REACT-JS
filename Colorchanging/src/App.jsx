693d44902d3fec2768581da0a8c1807c22966ae1
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-01-31 11:15:47 +0530

    bg color changing project

--- Colorchanging/src/App.jsx
+++ Colorchanging/src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ 
 
 function App() {
-  const [Color,setColor]=useState ("white")
+  const [color,setColor]=useState("black")
 
   return (
-    <>
-       <div className='W-full'></div>
-    </>
+    
+       <div className="w-full  h-screen  duration-200" style={{backgroundColor:color}}
+       > 
+       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'> 
+       <div className='flex flex-wrap justify-center gap-3 shadow-lg   bg-white px-3 py-2 rounded-2xl'>
+        <button onClick={function(){setColor("red")}} className='outline-none px-4 text-white px-4 py-2 rounded-2xl ' style={{backgroundColor:'red'}}
+        >Red</button>
+        <button  onClick={function(){setColor("yellow")}}   className='outline-none px-4 text-white px-4 py-2 rounded-2xl ' style={{backgroundColor:'yellow'}}
+        >yellow</button>
+        <button onClick={function(){setColor("green")}}  className='outline-none px-4 text-white px-4 py-2 rounded-2xl ' style={{backgroundColor:'green'}}
+        >green</button>
+        <button onClick={function(){setColor("blue")}}  className='outline-none px-4 text-white px-4 py-2 rounded-2xl ' style={{backgroundColor:'blue'}}
+        >blue</button>
+        <button onClick={function(){setColor("black")}}  className='outline-none px-4 text-white px-4 py-2 rounded-2xl ' style={{backgroundColor:'black'}}
+        >black</button>
+       </div>
+       </div>
+       </div>
+    
   )
 }
 
