4fc73a6e9b5001433ff76061fbddfbfc4a2e8cec
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-01 15:05:01 +0530

    yes

--- Passwdgenerator/src/App.jsx
+++ Passwdgenerator/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState ,useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState ,useCallback,useEffect ,useRef } from 'react'
+ 
 import './App.css'
+
 
 function App() {
   const [lenght,setLength]=useState(8)
-  const [numberAllow,setnumberAllow] =useState(false)
+  const [numberAllow,setNumberAllow] =useState(false)
   const [charAllow,setCharAllow]=useState(true)
   const [passward ,setPassward]=useState("")
+
+
+  //use ref hook
+  const passwardRef=useRef(null)
+
 
   const passwardGenerator= useCallback(()=>{
     let pass=""
@@ -16,38 +21,92 @@ function App() {
     if(numberAllow) str+="0123456789"
     if(charAllow) str+="!@##%^&*(){}?+-*"
 
-    for (let i = 1; i <= array.lenght; i++) 
+    for (let i = 1; i <=lenght; i++) 
     { 
-      let char=(math.floor(math.random()*str.length+1))
-      pass=str.charAt(char)
+      let char=(Math.floor(Math.random()*str.length+1))
+      
+      pass+=str.charAt(char)
     }
     setPassward(pass)
 
 
   },[lenght,numberAllow, charAllow,setPassward])
 
+  const copyPasswardToClipBoard=useCallback(()=>{
+    window.navigator.clipboard.writeText(passward)
 
+  },[passward])
+
+useEffect(()=>{passwardGenerator()},
+[lenght,numberAllow, charAllow,passwardGenerator])
+// passwardGenerator()
 
   return (
     <>
 
       
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-10 text-orange-500  bg-gray-700'>
-      <h1 className=' text-4xl text-center text-white margin-3'>Passward Generator</h1>
+      <h1 className=' text-4xl text-center text-white margin-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
         type="text"
         value={passward}
         className='outline-none, w-full py-1 px-3'
         placeholder='Password'
+        ref={passwardRef}
         readOnly
         />
+        <button 
+        onClick={copyPasswardToClipBoard}>
+
+        </button>
         <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink'>copy</button>
 
 
 
         </div>
-      
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="Range"
+          min={6}
+          max={100} 
+          value={lenght}
+          className='curser-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label >Length: {lenght}</label>
+        </div>
+
+      <div className='flex item-center gap-x-1'>
+      <input
+          type="checkbox"
+          defaultChecked={numberAllow}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllow((prev) => !prev);
+          }}
+      />
+        <label >Number</label>
+      </div>
+
+
+
+      <div className='flex item-center gap-x-1'>
+      <input
+          type="checkbox"
+          defaultChecked={charAllow}
+          // id="numberInput"
+          onChange={() => {
+              setCharAllow((prev) => !prev);
+          }}
+      />
+        <label >char</label>
+      </div>
+
+
+
+
+      </div>
       
       </div>
     
