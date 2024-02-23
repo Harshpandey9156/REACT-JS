df7d5a1726127f5ffb63f256a0ab4eadf4497a50
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-02-23 20:54:28 +0530

    Theme

---
+++ ThemeChange/src/Context/UserContextProvider.js
@@ -0,0 +1,18 @@
+
+
+
+import {createContext ,useContext} from 'react'
+
+export const ThemeContext=createContext({
+    themeMode:"light",
+    lightTheme :()=>{},
+    darkTheme: ()=>{}
+
+})
+export const ThemeProvider =ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+
+
+}
