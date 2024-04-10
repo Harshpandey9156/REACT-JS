30b322f99963efe4d1154a30d09877752a0b61d0
Harshpandey9156 <Harshpandey21012@gmail.com>
2024-04-10 15:20:51 +0530

    Input filed

--- MegaVlog/src/components/inputs.jsx
+++ MegaVlog/src/components/inputs.jsx
@@ -4,10 +4,24 @@ import React  from 'react'
  const input =React.forwardRef(function input({
 label ,
 type = 'text',
-className = '',   
- }){
-    return
+className = '', 
+...props  
+ },ref){
+   const id=useId()
+    return (
+      <div className="w-full">
+
+         {label && <label className="block mb-1 pl-1" htmlFor="{id}">
+            {label}
+         </label>}
+         <input
+            id={id}
+            type={type}
+            className={`w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500 ${className}`}
+            ref={ref}> </input>
+      </div>
+    )
  }
 )
 
-export default inputs
+export default input
